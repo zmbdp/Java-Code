3e1a94d81f0ba03b59a09e900d2c18b8d54a1946
HloveF <118424032+HloveF@users.noreply.github.com>
2023-04-05 11:12:43 +0800

    Create 打印个人信息.java

---
+++ 打印个人信息.java
@@ -0,0 +1,26 @@
+Public class shuchuxinxi{
+	public static void main(string[] args){
+		//姓名：
+		string name = "椎名真白";
+		system.out.println("姓名:" + "\t" + "\t" + name);
+		//年龄：
+		int age = 19;
+		system.out.println("年龄:" + "\t" + "\t" + age);
+		//性别：
+		char gender = '男';
+		system.out.println("性别:" + "\t" + "\t" + gender);
+		//身高：
+		double stature = 1.80;
+		system.out.println("身高:" + "\t" + "\t" + stature);
+		//是否单身：
+		boolean state  = false;
+		if(state == true)
+		{
+			System.out.println("是否单身:" + "\t" + "yes");
+		}
+		Else
+		{
+			System.out.println("是否单身:" + "\t" + "no");
+		}
+	}
+}
