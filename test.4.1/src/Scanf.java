0656765986a42a1adeae813420e7de7a025cf4d3
hlovef <3566983494@qq.com>
2023-04-16 12:13:15 +0800

    Java测试代码

---
+++ test.4.1/src/Scanf.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Scanf{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入:");
+        int s = sc.nextInt();//这个才是输入语句
+        System.out.println("s == " + s);
+    }
+}
