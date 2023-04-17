6d6775daa5c8533a7c2aa279d5d85fd1935ab27c
hlovef <3566983494@qq.com>
2023-04-17 21:13:54 +0800

    第二天作业

---
+++ Day02_homework.4.1/Day02_homework.java
@@ -0,0 +1,52 @@
+
+
+public class Day02_homework {
+    public static void main(String[] args) {
+//        //1. 小明账户bankBalance初始值有500.00元
+//        double money = 500.00;
+//        //2. 存入250元
+//        money += 250;
+//        //3. 支出100元
+//        money -= 100;
+//        //4. 打印剩余存款
+//        System.out.println(money);
+
+
+
+//        //1、班级里有100个学员。
+//        int i = 100;
+//        System.out.println("班级里有" + i + "个学生");
+//        //2、某商品价格55.55元。
+//        double q = 55.55;
+//        System.out.println("该商品价格为" + q + "元");
+//        //3、地球已经诞生超过46亿年。
+//        long year = 4600000000L;
+//        System.out.println("地球已诞生" + year + "年");
+//        //4、“5是偶数”这句话的描述是假的。
+//        if (5 % 2 == 0)
+//        {
+//            System.out.println("5是偶数吗?->ture");
+//        }
+//        else
+//        {
+//            System.out.println("5是偶数吗?->flase");
+//        }
+
+
+//        我的姓名是：张三
+        String name = "张三";
+//        我的性别是：男
+        char gender = '男';
+//        我的年龄是：18
+        int age = 18;
+//        我的地址是：xx省xx市
+        String address = "上海省上海市";
+//        我的饭卡余额是：55.55元
+        double money = 55.55;
+        System.out.println("我的姓名是：" + name);
+        System.out.println("我的性别是：" + gender);
+        System.out.println("我的年龄是：" + age);
+        System.out.println("我的地址是：" + address);
+        System.out.println("我的饭卡余额是：" + money);
+    }
+}
